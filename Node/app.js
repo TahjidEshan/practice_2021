e5f18cea56a958332e5dcbe8d44f044e6aab7f5b
TahjidEshan <atahjid@gmail.com>
2021-09-22 10:13:27 -0600

    start

---
+++ Node/app.js
@@ -0,0 +1,10 @@
+//console.log('Hello from Node');
+
+// const tutorial = require('./tutorial');
+// console.log(tutorial);
+// console.log(tutorial(1,1));
+
+const tutorial = require('./tutorial');
+console.log(tutorial.sum(1,1));
+console.log(tutorial.PI);
+console.log(new tutorial.SomeMathObject());
