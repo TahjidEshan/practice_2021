d7539378b1d5ab053324179932531032b6b5a4eb
TahjidEshan <atahjid@gmail.com>
2021-09-22 16:26:09 -0600

    k

--- Node/app.js
+++ Node/app.js
@@ -44,32 +44,62 @@
 // pedro.emit('name');
 // eshan.emit('name');
 
-const readline = require('readline');
-const rl = readline.createInterface({input : process.stdin, 
-                          output : process.stdout});
-let num1 = Math.floor((Math.random()*10)+1);
-let num2 = Math.floor((Math.random()*10)+1);
-let answer = num1 + num2;
+// const readline = require('readline');
+// const rl = readline.createInterface({input : process.stdin, 
+//                           output : process.stdout});
+// let num1 = Math.floor((Math.random()*10)+1);
+// let num2 = Math.floor((Math.random()*10)+1);
+// let answer = num1 + num2;
 
-rl.question(`What is ${num1} + ${num2}? \n`,
-(userinput)=> {
-    console.log(userinput);
-    if(userinput.trim() == answer){
-        rl.close();
-    }else{
-        rl.setPrompt('Incorrect response, please try again \n');
-        rl.prompt();
-        rl.on('line', (userinput)=>{
-            if(userinput.trim() == answer){
-                rl.close();
-            }else{
-                rl.setPrompt(`Your answer ${userinput} is wrong, try again \n`);
-                rl.prompt();
-            }
-        })
-    }
-});
+// rl.question(`What is ${num1} + ${num2}? \n`,
+// (userinput)=> {
+//     console.log(userinput);
+//     if(userinput.trim() == answer){
+//         rl.close();
+//     }else{
+//         rl.setPrompt('Incorrect response, please try again \n');
+//         rl.prompt();
+//         rl.on('line', (userinput)=>{
+//             if(userinput.trim() == answer){
+//                 rl.close();
+//             }else{
+//                 rl.setPrompt(`Your answer ${userinput} is wrong, try again \n`);
+//                 rl.prompt();
+//             }
+//         })
+//     }
+// });
 
-rl.on('close', () => {
-    console.log('Correct!!');
-});
+// rl.on('close', () => {
+//     console.log('Correct!!');
+// });
+
+const fs = require('fs');
+// //create a file
+// fs.writeFile('example.txt', "this example text", (err) => {
+//     if(err) console.log(err);
+//     else console.log('File created');
+
+//     fs.readFile('example.txt', 'utf8' ,(err, file) => {
+//         if(err) console.log(err);
+//         else console.log(file);
+//     });
+// });
+
+//rename file
+// fs.rename('example.txt', 'example2.txt', (err)=>{
+//     if(err) console.log(err);
+//     else console.log("Rename success");
+// });
+
+//append to file
+// fs.appendFile('example2.txt', 'new data appended', (err)=> {
+//     if(err) console.log(err);
+//     else console.log('Append success');
+// });
+
+//delete file
+// fs.unlink('example2.txt', (err)=>{
+//     if(err) console.log(err);
+//     else console.log('delete success');
+// });
